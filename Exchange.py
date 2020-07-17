25d3abdd144973707166484799896bf5f16a5426
AmbSheep <creamghost@mail.ru>
2020-07-17 19:07:28 +0300

    better function

--- Exchange.py
+++ Exchange.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
 import sys
-def countChange(amount):
-    return cc(amount, 3)
-def cc(amount, kindsOfCoins):
-    if amount==0:
-        return 1
-    if amount<0 or kindsOfCoins==0:
-        return 0
-    else:
-        return cc(amount, kindsOfCoins-1)+cc(amount-firstDenomination(kindsOfCoins), kindsOfCoins)
-def firstDenomination(kindsOfCoins):
-    if kindsOfCoins==1:
-        return 1
-    elif kindsOfCoins==2:
-        return 3
-    else:
-        return 5
-if __name__=='__main__':
-    print(countChange(sys.argv[1]))
+
+def solutions(amount):
+    coins = [1, 3, 5]
+    ways = [1] + [0] * amount
+    for coin in coins:
+        for i in range(coin, amount + 1):
+            ways[i] += ways[i - coin]
+    return ways[amount]
+
+
+if __name__ == '__main__':
+    print(solutions(int(sys.argv[1])))
